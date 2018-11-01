74ee6c3fa9a825f51d44bc6064d71c4aa11972ad
chenshenchao <chenshenchao@outlook.com>
2018-11-02 01:57:13 +0800

    amend TaskView.

--- src/view/TaskView.xaml.cs
+++ src/view/TaskView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
 
 namespace Wingale.View
 {
@@ -24,7 +26,43 @@ namespace Wingale.View
         public TaskView()
         {
             InitializeComponent();
-            table.ItemsSource = Process.GetProcesses();
+            table.ItemsSource = new ObservableCollection<Process>( Process.GetProcesses());
+        }
+
+        private void OnSearch(object sender, RoutedEventArgs e)
+        {
+            List<Process> result = new List<Process>();
+            Regex pattern = new Regex(keyword.Text);
+            foreach (Process process in Process.GetProcesses())
+            {
+                Match match = pattern.Match(process.ProcessName);
+                if (match.Success) result.Add(process);
+            }
+            table.ItemsSource = new ObservableCollection<Process>(result);
+        }
+
+        private void OnUpdate(object sender, RoutedEventArgs e)
+        {
+            table.ItemsSource = new ObservableCollection<Process>(Process.GetProcesses());
+        }
+
+        private void OnSort(object sender, RoutedEventArgs e)
+        {
+            ObservableCollection<Process> processes = table.ItemsSource as ObservableCollection<Process>;
+            GridViewColumnHeader header = e.OriginalSource as GridViewColumnHeader;
+            switch (header.Content) {
+                case "PID":
+                    table.ItemsSource = new ObservableCollection<Process>(processes.OrderBy(i => i.Id));
+                    break;
+                case "Name":
+                    table.ItemsSource = new ObservableCollection<Process>(processes.OrderBy(i => i.ProcessName));
+                    break;
+            }
+        }
+
+        private void OnSelect(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }

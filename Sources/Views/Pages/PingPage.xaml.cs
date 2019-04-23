c0247ac0667a65f67aac53b49b798f261109746b
chenshenchao <chenshenchao@outlook.com>
2019-04-24 00:44:11 +0800

    ip page.

--- Sources/Views/Pages/PingPage.xaml.cs
+++ Sources/Views/Pages/PingPage.xaml.cs
@@ -2,10 +2,19 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using Wingale.Utils;
 
 namespace Wingale.Views.Pages
 {
+    public class PingInfo
+    {
+        public IPAddress IP { get; set; }
+        public IPAddress ReplyIP { get; set; }
+        public IPStatus Status { get; set; }
+        public long Time { get; set; }
+    }
     public partial class PingPage : Page
     {
         public PingPage()
@@ -18,10 +27,21 @@ namespace Wingale.Views.Pages
             IPAddress ip = IPAddress.Parse(address.Text);
             IPAddress mask = IPAddress.Parse(netmask.Text);
             IPAddress next = ip.Next();
-            long count = mask.Count();
-            IPAddress net = ip.GetNet(mask);
-            info.Text = string.Format("net:{0} count: {1}",net, count);
             address.Text = next.ToString();
+            Task task = new Task(() => {
+                Ping ping = new Ping();
+                PingReply reply = ping.Send(ip);
+                Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    replyList.Items.Add(new PingInfo {
+                        IP = ip,
+                        Status = reply.Status,
+                        ReplyIP = reply.Address,
+                        Time = reply.RoundtripTime
+                    });
+                }));
+            });
+            task.Start();
         }
     }
 }
